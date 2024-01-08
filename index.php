b6d6cdbd132b3672f658e9a8c87a844a429ca10c
Garner Hernani <garner.hernani@newfold.com>
2024-01-08 15:28:25 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- include style -->
+    <?php include 'style.php'; ?>
+    <title>FDCI | Front-End Developer Technical Assessment</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="inner-wrap">
+
+        <div class="banner">
+            <img src="img/banner.png">
+        </div>
+
+        <div class="section_1">
+            <h1>プレミアムプラン（6,480円）に移行で <br>Amazonギフト券<span>5,000</span>円分プレゼント！</h1>
+            <img src="img/ribbon.jpg">
+            <p>2023年2月1日（水)　～　2023年2月28日（火）</p>
+        </div>
+
+        <div class="lead">
+            <p>申請〜受取までの流れ</p>
+        </div>
+
+        <div class="section_2">
+            <div class="inner-wrapper">
+
+                <div class="row">
+                    <div class="custom-col">
+                        <h2>1</h2>
+                        <p>キャンペーン期間内に新規登録を完了し、7日後プレミアム プラン（6,480円）に移行する。</p>
+                    </div>
+                    <div class="custom-col">
+                    <img src="img/img_step1.png">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="custom-fluid">
+                        <h2>2</h2>
+                        <p>10月7日までに下記申請フォームから、申し込みを完了してください。</p>
+                        <a href="javascript:;" class="button">Amazonギフト券申請</a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="custom-col">
+                        <h2>3</h2>
+                        <p>申請後、約2週間程度でAmazonよりギフトコード付与の メールが届きます。</p>
+                    </div>
+                    <div class="custom-col">
+                    <img src="img/img_step3.png">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="custom-col">
+                        <h2>4</h2>
+                        <p>Amazonからのメール内容に従い操作いただくと、ご自身のAmazonアカウントにギフト券が付与されます。</p>
+                    </div>
+                    <div class="custom-col">
+                    <img src="img/img_step4.png">
+                    </div>
+                </div>
+                
+                <div class="list">
+                    <p>▼応募資格</p>
+                    <ul class="round">
+                        <li>初めてネイティブキャンプに登録され、7日間無料トライアルに登録後、プレミアムプラン（6,480円）に移行済みの方。</li>
+                        <li>キャンペーン期間内に新規登録をし、10月7日までに申請を完了した方。</li>
+                        <li>当月中に無料トライアルを開始し、翌月にプレミアムプランに移行された場合。</li>
+                    </ul>
+
+                    <ul class="cross">
+                        <li>過去に一度登録済みの方。</li>
+                        <li>期限を過ぎて申請された方。</li>
+                        <li>新規登録後、無料トライアル期間中に退会された方。</li>
+                        <li>ファミリープラン・法人プラン・優待プランの方。</li>
+                        <li>お一人様で2回目以降の申請。(※お一人様につき1回までのため）</li>
+                    </ul>
+
+                    <p>▼ご注意事項</p>
+                    <ul class="dot">
+                        <li>7日間無料トライアル期間中に退会のお手続きをされない限り、自動でプレミアムプランに移行します。</li>
+                        <li>プレミアムプランに移行する際、メールでのご案内は行っておりません。<a href="javascript:;">ご利用明細</a>画面よりご確認ください。</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+    
+    <!-- include scripts -->
+    <?php include 'scripts.php'; ?>
+</body>
+</html>
